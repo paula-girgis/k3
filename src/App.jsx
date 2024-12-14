538958ec5b17fe976c2808dcf01f347c6b37d7b4
paula-girgis <143644246+paula-girgis@users.noreply.github.com>
2024-12-14 14:54:36 +0200

    Update 3 App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import ForgetPassword from './Components/ForgetPassword/ForgetPassword'
 import VerificationCode from './Components/VerificationCode/VerificationCode'
 import NewPassword from './Components/NewPassword/NewPassword'
 import PlantDetails from './Components/PlantDetails/PlantDetails'
-import EmailConfirmation from './Components/EmailConfirmation/emailConfirmation'
+import EmailConfirmation from './Components/emailConfirmation/emailConfirmation'
 
 
 const router = createBrowserRouter([
