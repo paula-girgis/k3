56c264aab477cbb48dff563421de4f5f468ab2ca
paula-girgis <143644246+paula-girgis@users.noreply.github.com>
2024-12-16 01:14:39 +0200

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,126 +1,77 @@
 
 
-import { NavLink, useNavigate } from 'react-router-dom';
+import React from 'react'
 import plantCareLogo from '../../assets/gradMaterial/logo.jpg';
-import { useContext, useState } from 'react';
-import { UserContext } from '../../Context/UserContext';
 
-export default function Navbar() {
-  let navigate = useNavigate();
-  let { userLogin, setUserLogin } = useContext(UserContext);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  function LogOut() {
-    localStorage.removeItem('userToken');
-    setUserLogin(null);
-    navigate('/login');
-  }
-
+export default function Footer() {
   return (
-    <>
-      <div className="navdiv flex items-center justify-center shadow-2xl px-2 sm:px-6 lg:px-28 pb-4">
-        <nav className="navIn shadow-2xl bg-gray-100 rounded-full flex items-center justify-between ps-4 lg:w-auto relative">
-          
+    <footer className="bgColor p-8 w-full shadow-lg">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-gray-900">
 
-          <div className="flex items-center">
-            <img className="h-16 lg:h-20 w-30" src={plantCareLogo} alt="plantCareLogo"/>
-    
+        {/* Logo and Description Section */}
+        <div className="text-center md:text-left mb-6 md:mb-0">
+          <div className="flex items-center justify-center md:justify-start">
+            <img src={plantCareLogo} alt="PlantCare" className="w-36 h-36" />
           </div>
+        </div>
 
-
-          {/* Toggle Button */}
-          <button
-            className="block lg:hidden text-green-900 text-2xl ml-auto pe-6"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <i className={`fa-solid ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
-          </button>
-
-          {/* Navbar Links */}
-          <ul
-            className={`absolute lg:static top-0  left-0 w-full lg:w-auto bg-gray-50 lg:bg-transparent lg:flex lg:space-x-3 lg:mx-32 rounded-xl lg:rounded-none p-10 lg:p-0 z-10 transition-all ${
-              isMenuOpen ? 'block' : 'hidden'
-            }`}
+        {/* Social Media Icons */}
+        <div className="flex space-x-6 mb-6 md:mb-0">
+          <a
+            href="https://www.instagram.com/plantcarehti/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-600 hover:text-white bg-gray-200 hover:bg-green-800 rounded-full p-3 transition ease-in-out duration-300"
           >
-            {userLogin !== null ? (
-              <>
-                {/* Links */}
-                <li className="text-center my-2">
-                  <NavLink to="home" onClick={() => setIsMenuOpen(false)}>
-                    Home
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="library" onClick={() => setIsMenuOpen(false)}>
-                    Library
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="eviroAnalysis" onClick={() => setIsMenuOpen(false)}>
-                    EviroAnalysis
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="floraCheck" onClick={() => setIsMenuOpen(false)}>
-                    FloraCheck
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="chatbot" onClick={() => setIsMenuOpen(false)}>
-                    Chatbot
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="aboutUs" onClick={() => setIsMenuOpen(false)}>
-                    AboutUs
-                  </NavLink>
-                </li>
-                <li className="text-center my-2">
-                  <NavLink to="contactUs" onClick={() => setIsMenuOpen(false)}>
-                    ContactUs
-                  </NavLink>
-                </li>
+            <i className="fa-brands fa-facebook-f"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/plantcarehti/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-600 hover:text-white bg-gray-200 hover:bg-green-800 rounded-full p-3 transition ease-in-out duration-300"
+          >
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/plantcarehti/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-600 hover:text-white bg-gray-200 hover:bg-green-800 rounded-full p-3 transition ease-in-out duration-300"
+          >
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/plantcarehti/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-600 hover:text-white bg-gray-200 hover:bg-green-800 rounded-full p-3 transition ease-in-out duration-300"
+          >
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+        </div>
 
-                {/* Logout Icon */}
-                <li
-                  className="text-center my-2 lg:hidden cursor-pointer text-red-900 text-lg "
-                  onClick={LogOut}>
-                  <i className="fa-solid fa-right-from-bracket "></i>
-                </li>
-              </>
-            ) : (
-              <>
-                {/* Auth Buttons for Small Screens */}
-                <li className="text-center my-2">
-                  <NavLink to="login" onClick={() => setIsMenuOpen(false)}>
-                    <button className="border border-green-900 text-green-900 px-4 py-2 rounded-full hover:bg-green-50 text-sm lg:text-base">
-                      SignIn
-                    </button>
-                  </NavLink>
-                </li>
-                <li className="text-center my-2 ">
-                  <NavLink to="register" onClick={() => setIsMenuOpen(false)}>
-                    <button className="bg-green-900 text-white px-5 py-2 rounded-full hover:bg-green-700 text-sm lg:text-base">
-                      SignUp
-                    </button>
-                  </NavLink>
-                </li>
-              </>
-            )}
-
-            {/* Close Button for Mobile */}
-            <li className="absolute top-3 right-3 lg:hidden cursor-pointer" onClick={() => setIsMenuOpen(false)}>
-              <i className="fa-solid fa-times text-2xl"></i>
+        {/* Contact Information Section */}
+        <div className="text-center md:text-right">
+          <ul className="mt-2 text-gray-600 text-sm">
+            <li className="flex items-center justify-center md:justify-start mt-2">
+              <i className="fa-solid fa-location-dot p-1" style={{ color: '#333' }}></i>
+              Cairo, Egypt
+            </li>
+            <li className="flex items-center justify-center md:justify-start mt-2">
+              <i className="fa-solid fa-envelope p-1" style={{ color: '#333' }}></i>
+              plantcare.hti@gmail.com
+            </li>
+       
+            <li className="flex items-center justify-center md:justify-start mt-2">
+              <i className="fa-solid fa-phone p-1" style={{ color: '#333' }}></i>
+              01204548235
             </li>
           </ul>
-
-          {/* Logout Icon for Large Screens */}
-          {userLogin !== null && (
-            <div className="hidden lg:block cursor-pointer text-red-900 text-lg pe-6" onClick={LogOut}>
-              <i className="fa-solid fa-right-from-bracket"></i>
-            </div>
-          )}
-        </nav>
+        </div>
       </div>
-    </>
+
+    
+    </footer>
   );
 }
